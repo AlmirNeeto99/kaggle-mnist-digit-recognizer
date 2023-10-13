499bd7c495bb69fbdd0eb86d84bd13a633c07790
Almir Neto <almirneto338@gmail.com>
2023-10-13 16:19:46 -0300

    Converting the data to a tensor

--- MNISTDataset.py
+++ MNISTDataset.py
@@ -1,18 +1,19 @@
+from torch import tensor, float32
 from torch.utils.data import Dataset
 import pandas as pd
 
-class MNISTDataset(Dataset):
 
-    def __init__(self, dataPath, train = True):
+class MNISTDataset(Dataset):
+    def __init__(self, dataPath, train=True):
         self.data = pd.read_csv(dataPath)
         self.train = train
 
     def __getitem__(self, idx):
         line = self.data.iloc[idx]
         if self.train:
-            return line.iloc[0], line.iloc[1:].to_list()
+            return line.iloc[0], tensor(line.iloc[1:].to_list(), dtype=float32)
         else:
-            return line.iloc[0:].to_list()
+            return tensor(line.iloc[0:].to_list(), dtype=float32)
 
     def __len__(self):
         return self.data.shape[0]
