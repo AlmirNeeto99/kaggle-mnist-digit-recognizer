ce79aedbd1f22e9db433aa829dfb9d58545c0250
Almir Neto <almirneto338@gmail.com>
2023-10-13 10:43:31 -0300

    Adding a way to change the return for train/test dataset

--- MNISTDataset.py
+++ MNISTDataset.py
@@ -3,12 +3,16 @@ import pandas as pd
 
 class MNISTDataset(Dataset):
 
-    def __init__(self, dataPath):
+    def __init__(self, dataPath, train = True):
         self.data = pd.read_csv(dataPath)
+        self.train = train
 
     def __getitem__(self, idx):
         line = self.data.iloc[idx]
-        return line.iloc[0], line.iloc[1:].to_list()
+        if self.train:
+            return line.iloc[0], line.iloc[1:].to_list()
+        else:
+            return line.iloc[0:].to_list()
 
     def __len__(self):
         return self.data.shape[0]
