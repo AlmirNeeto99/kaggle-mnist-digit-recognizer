231186ca586ba477399f7c3d00cf3d3198f50c25
Almir Neto <almirneto338@gmail.com>
2023-10-15 13:54:39 -0300

    Improving loading training dataset

--- MNISTDataset.py
+++ MNISTDataset.py
@@ -3,9 +3,32 @@ from torch.utils.data import Dataset
 import pandas as pd
 
 
-class MNISTDataset(Dataset):
-    def __init__(self, dataPath, train=True):
+class _PandasDataset(Dataset):
+    def __init__(self, dataPath):
         self.data = pd.read_csv(dataPath)
+
+
+class MNISTDatasetReader:
+    def __init__(self, path, division=0.7) -> None:
+        self.path = path
+        self.division = division
+        self.data = _PandasDataset(self.path)
+
+    def getDatasets(self):
+        dt = self.data.data
+
+        length = len(dt)
+        cutSize = length * self.division
+
+        if self.division == 1:
+            return dt
+
+        return dt.loc[:cutSize], dt.loc[cutSize:].reset_index(drop=True)
+
+
+class MNISTDataset(Dataset):
+    def __init__(self, pandas, train=True):
+        self.data = pandas
         self.train = train
 
     def __getitem__(self, idx):
