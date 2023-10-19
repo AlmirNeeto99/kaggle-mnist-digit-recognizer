eaf7eef875026053d0e8460dbb0e8e15196c8fe3
Almir Neto <almirneto338@gmail.com>
2023-10-18 23:27:29 -0300

    Returning to best model (98% accuracy)

--- MNISTClassifier.py
+++ MNISTClassifier.py
@@ -7,11 +7,10 @@ class MNISTClassifier(torch.nn.Module):
 
         self.h1 = torch.nn.Conv2d(1, 64, 3, 1)  # 26
         self.a1 = torch.nn.ReLU()
-        self.h2 = torch.nn.Conv2d(64, 32, 3, 1)  # 24
+        self.h2 = torch.nn.Conv2d(64, 128, 3, 1)  # 24
         self.a2 = torch.nn.ReLU()
-        self.p1 = torch.nn.MaxPool2d((2, 2), 2)  # 12
         self.f = torch.nn.Flatten()
-        self.out = torch.nn.Linear(32 * 12 * 12, 10)
+        self.out = torch.nn.Linear(128 * 12 * 12, 10)
         self.outA = torch.nn.Softmax(dim=0)
 
     def forward(self, x):
@@ -20,7 +19,6 @@ class MNISTClassifier(torch.nn.Module):
         x = self.a1(x)
         x = self.h2(x)
         x = self.a2(x)
-        x = self.p1(x)
         x = self.f(x)
         x = self.out(x)
         return self.outA(x)
