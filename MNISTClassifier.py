a46e68729377225bdb3277863bf1490e8fd84b8e
Almir Neto <almirneto338@gmail.com>
2023-11-07 01:27:21 -0300

    Adding a new layer

--- MNISTClassifier.py
+++ MNISTClassifier.py
@@ -5,17 +5,19 @@ class MNISTClassifier(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.h1 = torch.nn.Conv2d(1, 128, 3, 1)  # 26
+        self.h1 = torch.nn.Conv2d(1, 64, 3, 1)  # 26
         self.a1 = torch.nn.ReLU()
         self.p1 = torch.nn.MaxPool2d(2, stride=2)  # 13
-        self.h2 = torch.nn.Conv2d(128, 256, 3, 1)  # 11
+        self.h2 = torch.nn.Conv2d(64, 128, 3, 1)  # 11
         self.a2 = torch.nn.ReLU()
-        self.h3 = torch.nn.Conv2d(256, 512, 3, 1)  # 9
+        self.h3 = torch.nn.Conv2d(128, 256, 3, 1)  # 9
         self.a3 = torch.nn.ReLU()
+        self.h4 = torch.nn.Conv2d(256, 512, 3, 1) # 7
+        self.a4 = torch.nn.ReLU()
         self.f = torch.nn.Flatten()
-        self.out = torch.nn.Linear(512 * 9 * 9, 1000)
+        self.out = torch.nn.Linear(512 * 7 * 7, 10000)
         self.outA = torch.nn.ReLU()
-        self.out2 = torch.nn.Linear(1000, 10)
+        self.out2 = torch.nn.Linear(10000, 10)
         self.outA2 = torch.nn.LogSoftmax(dim=1)
 
     def forward(self, x):
@@ -27,6 +29,8 @@ class MNISTClassifier(torch.nn.Module):
         x = self.a2(x)
         x = self.h3(x)
         x = self.a3(x)
+        x = self.h4(x)
+        x = self.a4(x)
         x = self.f(x)
         x = self.out(x)
         x = self.outA(x)
